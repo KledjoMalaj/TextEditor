d9d85814ae0f03bdf0f0204ca8d78a8ba7ae54c9
Kledjo Malaj <kledjomalaj20@gmail.com>
2025-10-08 22:46:54 +0200

    TextEditor Project start

---
+++ TextEditor/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className={'flex justify-center'}>
+            <div contentEditable={"true"} className={"bg-gray-200 w-150 h-150 mt-25 border"}>
+
+            </div>
+        </div>
+   </>
+  )
+}
+
+export default App
