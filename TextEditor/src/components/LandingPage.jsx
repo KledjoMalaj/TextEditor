584726cae61010888f012082fa738a2ce5647a76
Kledjo Malaj <kledjomalaj20@gmail.com>
2025-10-13 22:27:34 +0200

    css upates

--- TextEditor/src/components/LandingPage.jsx
+++ TextEditor/src/components/LandingPage.jsx
@@ -31,19 +31,24 @@ function LandingPage() {
             {titlePopUp &&  <TitlePupUp onClose={()=>setTitlePopUP(false)}/>}
 
             <div className={'flex justify-center m-5'}>
-                <button className={'bg-gradient-to-r from-blue-500 to-blue-700 text-white p-2 rounded cursor-pointer'}
+                <button className={'bg-gradient-to-r from-blue-500 to-blue-700 text-white p-2 rounded cursor-pointer shadow-md'}
                 onClick={handleStartNew}>Start New</button>
             </div>
-            <div className={'grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 m-5 gap-5'}>
+
+
+            <div className={'grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 mx-5 gap-5'}>
 
                 {documents && documents.map(i=>
-                <div  key={i.id} className={'h-75 bg-white rounded shadow-md'}>
-                    <div className={'bg-blue-400 text-white h-10 p-2 rounded shadow-md flex justify-between'}>
-                        <h1 className={''}>{i.title}</h1>
-                        <button className={'bg-red-400 px-2 rounded cursor-pointer'} onClick={()=>handleDelete(i.id)}>Delete</button>
+                    <div   key={i.id}>
+                        <div className={'bg-blue-400 flex justify-between p-2 rounded-t'}>
+                            <h1 className={'text-white'}>{i.title}</h1>
+                            <button className={'bg-red-400 px-2 rounded cursor-pointer'} onClick={()=>handleDelete(i.id)}>Delete</button>
+                        </div>
+
+                        <div onClick={()=>handleOpenDoc(i.id)} className={'h-75 bg-white rounded shadow-lg cursor-pointer p-2'}>
+                            <div  className={'mt-5 px-5'} dangerouslySetInnerHTML={{ __html: i.content }}></div>
+                        </div>
                     </div>
-                    <div onClick={()=>handleOpenDoc(i.id)} className={'cursor-pointer mt-5 px-5'} dangerouslySetInnerHTML={{ __html: i.content }}></div>
-                </div>
                 )}
             </div>
         </>
