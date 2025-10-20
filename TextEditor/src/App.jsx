a19617d531c831e4c669d732a12b98b092406d7f
Kledjo Malaj <kledjomalaj20@gmail.com>
2025-10-20 18:42:40 +0200

    User Logout

--- TextEditor/src/App.jsx
+++ TextEditor/src/App.jsx
@@ -2,9 +2,10 @@ import './App.css'
 import Editor from "./components/Editor.jsx";
 import ToolBar from "./components/ToolBar.jsx";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from "./components/HomePage.jsx";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
 
@@ -12,8 +13,18 @@ function App() {
         <>
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
-                <Route path="/homePage" element={<HomePage />} />
-                <Route path="/editor/:id" element={<Editor />} />
+
+                <Route path="/homePage" element={
+                    <ProtectedRoute>
+                        <HomePage />
+                    </ProtectedRoute>
+                } />
+
+                <Route path="/editor/:id" element={
+                    <ProtectedRoute>
+                        <Editor />
+                    </ProtectedRoute>
+                } />
             </Routes>
         </>
     );
--- TextEditor/src/components/HomePage.jsx
+++ TextEditor/src/components/HomePage.jsx
@@ -5,19 +5,28 @@ import TitlePupUp from "./TitlePupUp.jsx";
 import {Bars3Icon} from "@heroicons/react/16/solid/index.js";
 import OptionsMenu from "./OptionsMenu.jsx";
 import { RxPlus } from "react-icons/rx";
+import { FaRegUser } from "react-icons/fa";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
 function HomePage() {
     const navigate = useNavigate()
     const [documents,setDocuments] = useState([])
     const [titlePopUp,setTitlePopUP] = useState(false)
     const [openMenuId, setOpenMenuId] = useState(null);
+    const [user,setUser] = useState([])
 
     useEffect(() => {
         axios.get('http://localhost:3030/Documents/getAll',{
             headers:{'Authorization':`Bearer ${localStorage.getItem('token')}`}
         })
             .then((res)=>setDocuments(res.data))
+
+        axios.get('http://localhost:3030/Users/me', {
+            headers:{'Authorization':`Bearer ${localStorage.getItem('token')}`}
+        })
+            .then((res)=>setUser(res.data))
     }, []);
+
 
     const handleOpenDoc = (id)=> {
         navigate(`/editor/${id}`)
@@ -39,31 +48,46 @@ function HomePage() {
         setDocuments(documents.map(doc => doc.id === id ? {...doc,title:newTitle } : doc))
 
         setOpenMenuId(null)
+    }
 
+    const handleLogOut = () => {
+        localStorage.removeItem('token');
+        navigate('/');
     }
 
     return(
         <>
             {titlePopUp &&  <TitlePupUp onClose={()=>setTitlePopUP(false)}/>}
 
-            <div className={'flex justify-center my-2 mx-3 mt-5'}>
-                <div className={'flex bg-white py-2 px-6 rounded'}>
+            <div className={'flex justify-between my-2 mx-3 mt-5 mb-4'}>
+
+                    <h1 className={'flex gap-2 bg-blue-700 rounded p-3 px-5  mt-1 mb-2 shadow-lg text-lg font-semibold text-white'}>
+                        <FaRegUser className={'h-6 w-6 text-white'}/>{user.email}
+                    </h1>
+
+                <div className={'flex bg-white py-2 px-6 rounded shadow-lg'}>
                     <h1 className={'pl-2 h-6 w-30 mt-2 font-semibold text-xl'}>Start New </h1>
                     <button className={'StartButton'}
                             onClick={handleStartNew}>
                         <RxPlus className={'h-6 w-6'}/>
                     </button>
                 </div>
+
+                    <button className={'text-lg font-semibold text-white flex gap-2 cursor-pointer bg-red-500 rounded shadow-lg p-3 px-5 mt-1 mb-2 hover:bg-red-600 hover:shadow-xl transition-all duration-150'}
+                    onClick={handleLogOut}>
+                        Log Out<RiLogoutBoxRLine className={'h-7 w-7 '}/>
+                    </button>
+
             </div>
 
-            <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-6 mx-3 bg-white rounded p-5">
+            <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-6 mx-3 bg-white rounded p-5 border">
                 {documents && documents.map(i => (
                     <div key={i.id} className="Document-card">
                         <div className={"Document-card2"}>
                             <div className="bg-blue-500 flex justify-between items-center p-3 rounded-t">
                                 <h1 className="text-white text-lg font-semibold">{i.title}</h1>
                                 <button
-                                    className={"cursor-pointer hover:bg-blue-700 px-3 py-2 rounded"}
+                                    className={"cursor-pointer hover:bg-blue-700 px-3 py-2 rounded transition-all duration-150"}
                                     onClick={() => setOpenMenuId(openMenuId === i.id ? null : i.id)}
                                 >
                                     <Bars3Icon className={"h-6 w-6 text-white "}/>
